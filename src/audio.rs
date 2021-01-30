d889b9498949edf4772ad745d1341b89ed317e97
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2021-01-30 19:09:18 +0100

    Audio

--- src/audio.rs
+++ src/audio.rs
@@ -1,38 +1,46 @@
 use fehler::throws;
 
-#[derive(derive_more::Deref, derive_more::DerefMut)] pub struct Output {
-	pub rate: u32,
-	#[deref]#[deref_mut] output: alsa::direct::pcm::MmapPlayback<i16>,
+//#[derive(derive_more::Deref, derive_more::DerefMut)]
+pub struct Output {
+	//pub poll: Vec<alsa::poll::pollfd>,
+	pub device: alsa::PCM,
+	//pub rate: u32,
+	//#[deref]#[deref_mut]
+	output: alsa::direct::pcm::MmapPlayback<i16>,
 }
 
-#[throws(anyhow::Error)] pub fn output() -> Output {
-	let device = alsa::PCM::new("hw:1", alsa::Direction::Playback, false)?;
+impl Output {
+#[throws(alsa::Error)] pub fn new() -> Self {
+	let device = alsa::PCM::new("hw:1,0", alsa::Direction::Playback, false)?;
 	{let hardware_parameters = alsa::pcm::HwParams::any(&device)?;
+		hardware_parameters.set_rate(44100, alsa::ValueOr::Nearest)?;
 		hardware_parameters.set_channels(2)?;
-		//hardware_parameters.set_rate(rate, alsa::ValueOr::Greater)?;
 		hardware_parameters.set_format(alsa::pcm::Format::s16())?;
 		hardware_parameters.set_access(alsa::pcm::Access::MMapInterleaved)?;
-		//hardware_parameters.set_buffer_size(2K)?;
-		//hardware_parameters.set_period_size(/2, alsa::ValueOr::Nearest)?;
+		hardware_parameters.set_buffer_size(hardware_parameters.get_buffer_size_max()?)?;
+		hardware_parameters.set_periods(2, alsa::ValueOr::Greater)?;
 		device.hw_params(&hardware_parameters)?;}
-	let hardware_parameters = device.hw_params_current()?;
+	{let hardware_parameters = device.hw_params_current()?;
 	{let software_parameters = device.sw_params_current()?;
 		software_parameters.set_avail_min(hardware_parameters.get_period_size()?)?;
-		device.sw_params(&software_parameters)?;}
-	let mut output = device.direct_mmap_playback::<i16>()?;
+		device.sw_params(&software_parameters)?;}}
 	device.prepare()?;
-	//device.avail() > 0
-	output.write(&mut std::iter::repeat(0));
-	//assert(device.status().state() == alsa::pcm::State::Prepared); //Running
-	device.start()?;
-	Output{rate: hardware_parameters.get_rate()?, output}
+	let output = device.direct_mmap_playback::<i16>()?;
+	Self{device, output}
 }
 
-pub fn write(output: &mut alsa::direct::pcm::MmapPlayback<i16>, frames: &mut impl ExactSizeIterator<Item=(i16, i16)>) -> usize {
-	let (buffer, _) = output.data_ptr();
+#[throws(alsa::Error)] pub fn write(&mut self, frames: &mut impl ExactSizeIterator<Item=(i16, i16)>) -> usize {
+	assert!(frames.len() > 0, "{}", frames.len());
+	let (buffer, _) = self.output.data_ptr();
 	let buffer = unsafe{std::slice::from_raw_parts_mut(buffer.ptr as *mut [i16; 2], buffer.frames as usize)};
+	assert!(buffer.len() > 0, "{}", buffer.len());
 	let target = buffer.into_iter().zip(frames);
 	let len = target.len();
 	for (target, frame) in target { unsafe{std::ptr::write_volatile(target as *mut [i16; 2], [frame.0, frame.1])}; }
-	{output.commit(len as alsa::pcm::Frames); len}
+	self.output.commit(len as alsa::pcm::Frames);
+	if self.output.status().state() == alsa::pcm::State::Prepared { self.device.start()?; }
+	assert!(self.output.status().state() == alsa::pcm::State::Running);
+	len
+}
+
 }
--- src/main.rs
+++ src/main.rs
@@ -13,29 +13,27 @@ use {anyhow::{Error, Result}, fehler::throws};
 
 mod audio;
 
-#[throws] fn play(mut output: &mut audio::Output, entry: &std::fs::DirEntry) {
+#[throws] fn play(output: &mut audio::Output, entry: &std::fs::DirEntry) {
 	use std::default::default;
 	let mut reader = symphonia::default::get_probe().format(&symphonia::core::probe::Hint::new().with_extension(entry.path().extension().unwrap().to_str().unwrap()),
 																																															symphonia::core::io::MediaSourceStream::new(Box::new(std::fs::File::open(entry.path())?)), &default(), &default())?.format;
 	let stream = reader.default_stream().unwrap();
 	let mut decoder = symphonia::default::get_codecs().make(&stream.codec_params, &default())?;
-	//assert_eq!(reader.streaminfo().sample_rate, output.rate);
 	while let Ok(packet) = reader.next_packet() {
-		match decoder.decode(&packet)? {
-			symphonia::core::audio::AudioBufferRef::S32(buffer) => {
-				use symphonia::core::audio::Signal;
-				//use itertools::Itertools; let mut samples = buffer.chan(0).into_iter().interleave_shortest(buffer.chan(1)).map(|v| v as i16); // impl ExactSizeIterator
-				//let mut samples = (0..buffer.frames()).flat_map(|i| [buffer.chan(0)[i], buffer.chan(1)[i]].into_iter()).map(|v| v as i16); // impl ExactSizeIterator
-				//let mut samples = (0..buffer.frames()).map(|i| [buffer.chan(0)[i], buffer.chan(1)[i]]
-				let mut samples = buffer.chan(0).into_iter().map(|&v| v as i16).zip(buffer.chan(1).into_iter().map(|&v| v as i16));
-				while samples.len() > 0 { 	audio::write(&mut output, &mut samples); }
-			},
-			_ => unimplemented!(),
+		#[throws] fn write<S: symphonia::core::sample::Sample>(output: &mut audio::Output, buffer: &symphonia::core::audio::AudioBuffer<S>) where i16: symphonia::core::conv::FromSample<S> {
+			assert_eq!(buffer.spec().rate, output.device.hw_params_current()?.get_rate()?);
+			use symphonia::core::audio::Signal;
+			let mut samples = buffer.chan(0).into_iter().map(|&v| symphonia::core::conv::FromSample::from_sample(v)).zip(buffer.chan(1).into_iter().map(|&v| symphonia::core::conv::FromSample::from_sample(v)));
+			while samples.len() > 0 {	 output.write(&mut samples)?; alsa::poll::poll(&mut alsa::PollDescriptors::get(&output.device)?, -1)?; } //alsa::poll::poll(&mut output.poll, -1)?; }
 		}
+		{use symphonia::core::audio::AudioBufferRef::*; match decoder.decode(&packet)? {
+			S32(buffer) => write(output, &buffer),
+			F32(buffer) => write(output, &buffer),
+		}}?;
 	}
 }
 
 #[throws] fn main() {
-	let mut output = audio::output()?;
+	let mut output = audio::Output::new()?;
 	visit(&std::env::current_dir()?, &mut move |entry| play(&mut output, entry))?
 }
